72bbe142265b82469291bd0f5ec035e0bbd77508
blake S <design.harls@icloud.com>
2023-03-23 19:10:30 +0100

    init project create folder

---
+++ src/main/java/ctecnica/example/App.java
@@ -0,0 +1,21 @@
+package ctecnica.example;
+
+// import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      
+        String entrada = " 0 h 2 3 5 1 v 3 1 1";
+        System.out.println(entrada);
+        // int option = 0;
+        // System.out.println(" 1. Eat \n 2. Play \n 3. Sleep \n 4. Poop \n 5. Status \n 6. Exit \n ");
+        // Scanner qwerty = new Scanner(System.in);
+        // option = Integer.parseInt(qwerty.next());
+    }
+}
---
+++ src/main/java/ctecnica/example/entity/Block.java
@@ -0,0 +1,97 @@
+package ctecnica.example.entity;
+
+public class Block {
+    // //atributos
+    // Integer length;
+    // Integer positionX;
+    // Integer positionY;
+    // Integer width = 1;
+    // Integer id = 0;
+    
+    // //contructores
+    // public Block(Integer length, Integer positionX, Integer positionY, Integer width, Integer id) {
+    //     this.length = length;
+    //     this.positionX = positionX;
+    //     this.positionY = positionY;
+    //     this.width = width;
+    //     this.id = id;
+    // }
+
+    // //getters y setters
+    // public Integer getLength() {
+    //     return length;
+    // }
+    
+    // public void setLength(Integer length) {
+    //     this.length = length;
+    // }
+
+    // public Integer getPositionX() {
+    //     return positionX;
+    // }
+
+    // public void setPositionX(Integer positionX) {
+    //     this.positionX = positionX;
+    // }
+
+    // public Integer getPositionY() {
+    //     return positionY;
+    // }
+
+    // public void setPositionY(Integer positionY) {
+    //     this.positionY = positionY;
+    // }
+
+    // public Integer getWidth() {
+    //     return width;
+    // }
+
+    // public void setWidth(Integer width) {
+    //     this.width = width;
+    // }
+
+    // public Integer getId() {
+    //     return id;
+    // }
+
+    // public void setId(Integer id) {
+    //     this.id = id;
+    // }
+
+
+    //methods
+    // tiene que devolvernos un true si se superponen y false en caso contrario.
+    // public  static boolean overlap(String bloque1, String bloque2){
+    //     // bloque 1
+    //     String parametro1 = "0 h 2 3 5 1";
+    //     String [] espacio = parametro1.split(parametro1);
+
+    //     Integer idBloque1;
+    //     String orientacionBloque1;
+    //     Integer ejeXbloque1;
+    //     Integer ejeYbloque1;
+    //     Integer lengthBloque1;
+
+
+    //     //bloque 2
+    //     String parametro2 = "v 3 1 1";
+    //     String [] espacio2 = parametro2.split(parametro2);
+    //     Integer idBloque2;
+    //     String orientacionBloque2;
+    //     Integer ejeXbloque2;
+    //     Integer ejeYbloque2;
+    //     Integer lengthBloque2;
+
+    // }
+    
+
+    
+   
+  
+
+
+
+   
+
+
+}
