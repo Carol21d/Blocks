12b2dac268d5f1414e8e7195b952e227732ad01a
blake S <design.harls@icloud.com>
2023-03-23 22:25:05 +0100

    create folder entity with class block

--- src/main/java/ctecnica/example/Game.java
+++ src/main/java/ctecnica/example/Game.java
@@ -7,4 +7,32 @@ public class Game {
 
     }
 
+
+
+
+     //methods
+    // tiene que devolvernos un true si se superponen y false en caso contrario.
+    // public  static boolean overlap(String bloque1, String bloque2){
+    //     // bloque 1
+    //     String parametro1 = "0 h 2 3 5 1";
+    //     String [] espacio = parametro1.split(parametro1);
+
+    //     Integer idBloque1;
+    //     String orientacionBloque1;
+    //     Integer ejeXbloque1;
+    //     Integer ejeYbloque1;
+    //     Integer lengthBloque1;
+
+
+    //     //bloque 2
+    //     String parametro2 = "v 3 1 1";
+    //     String [] espacio2 = parametro2.split(parametro2);
+    //     Integer idBloque2;
+    //     String orientacionBloque2;
+    //     Integer ejeXbloque2;
+    //     Integer ejeYbloque2;
+    //     Integer lengthBloque2;
+
+    // }
+    
 }
--- src/main/java/ctecnica/example/entity/Block.java
+++ src/main/java/ctecnica/example/entity/Block.java
@@ -1,89 +1,87 @@
 package ctecnica.example.entity;
 
+
+
 public class Block {
-    // //atributos
-    // Integer length;
-    // Integer positionX;
-    // Integer positionY;
-    // Integer width = 1;
-    // Integer id = 0;
+    //atributos
+    Integer length;
+    String orientacion;
+    Integer positionX;
+    Integer positionY;
+    Integer width = 1;
+    Integer id = 0;
+
+    //constructores
+    public Block(Integer length, String orientacion, Integer positionX, Integer positionY, Integer width, Integer id) {
+        this.length = length;
+        this.orientacion = orientacion;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.id = id;
+    }
     
-    // //contructores
-    // public Block(Integer length, Integer positionX, Integer positionY, Integer width, Integer id) {
-    //     this.length = length;
-    //     this.positionX = positionX;
-    //     this.positionY = positionY;
-    //     this.width = width;
-    //     this.id = id;
-    // }
 
-    // //getters y setters
-    // public Integer getLength() {
-    //     return length;
-    // }
+
     
-    // public void setLength(Integer length) {
-    //     this.length = length;
-    // }
+    //getters y setters
+    public Integer getLength() {
+        return length;
+    }
 
-    // public Integer getPositionX() {
-    //     return positionX;
-    // }
+    public void setLength(Integer length) {
+        this.length = length;
+    }
 
-    // public void setPositionX(Integer positionX) {
-    //     this.positionX = positionX;
-    // }
+    public String getOrientacion() {
+        return orientacion;
+    }
 
-    // public Integer getPositionY() {
-    //     return positionY;
-    // }
+    public void setOrientacion(String orientacion) {
+        this.orientacion = orientacion;
+    }
 
-    // public void setPositionY(Integer positionY) {
-    //     this.positionY = positionY;
-    // }
+    public Integer getPositionX() {
+        return positionX;
+    }
 
-    // public Integer getWidth() {
-    //     return width;
-    // }
+    public void setPositionX(Integer positionX) {
+        this.positionX = positionX;
+    }
 
-    // public void setWidth(Integer width) {
-    //     this.width = width;
-    // }
+    public Integer getPositionY() {
+        return positionY;
+    }
 
-    // public Integer getId() {
-    //     return id;
-    // }
+    public void setPositionY(Integer positionY) {
+        this.positionY = positionY;
+    }
 
-    // public void setId(Integer id) {
-    //     this.id = id;
-    // }
+    public Integer getWidth() {
+        return width;
+    }
 
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
 
-    //methods
-    // tiene que devolvernos un true si se superponen y false en caso contrario.
-    // public  static boolean overlap(String bloque1, String bloque2){
-    //     // bloque 1
-    //     String parametro1 = "0 h 2 3 5 1";
-    //     String [] espacio = parametro1.split(parametro1);
+    public Integer getId() {
+        return id;
+    }
 
-    //     Integer idBloque1;
-    //     String orientacionBloque1;
-    //     Integer ejeXbloque1;
-    //     Integer ejeYbloque1;
-    //     Integer lengthBloque1;
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
 
-    //     //bloque 2
-    //     String parametro2 = "v 3 1 1";
-    //     String [] espacio2 = parametro2.split(parametro2);
-    //     Integer idBloque2;
-    //     String orientacionBloque2;
-    //     Integer ejeXbloque2;
-    //     Integer ejeYbloque2;
-    //     Integer lengthBloque2;
-
-    // }
     
+
+    
+    
+   
+
+
+   
 
     
    
